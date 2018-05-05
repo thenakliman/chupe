3210e979a72bc0208a6771ef4d44c74b4b9083bf
Rajiv Kumar <thenakliman@gmail.com>
2018-05-05 12:09:15 +0530

    [thenakliman] #12 Added model for the question database

---
+++ src/main/java/org/thenakliman/chupe/models/Question.java
@@ -0,0 +1,80 @@
+package org.thenakliman.chupe.models;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Question {
+  @Id
+  @Column(name = "id", nullable = false, unique = true)
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  @Column(name = "question")
+  private String question;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "owner")
+  private String owner;
+
+  @Column(name = "assignedTo")
+  private String assignedTo;
+
+  @Column(name = "createdAt")
+  private Date createdAt;
+
+  @Column(name = "updated_at")
+  private Date updatedAt;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(String question) {
+    this.question = question;
+  }
+
+  public Date getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public String getAssignedTo() {
+    return assignedTo;
+  }
+
+  public void setAssignedTo(String assignedTo) {
+    this.assignedTo = assignedTo;
+  }
+
+  public String getOwner() {
+    return owner;
+  }
+
+  public void setOwner(String owner) {
+    this.owner = owner;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
---
+++ src/main/java/org/thenakliman/chupe/repositories/QuestionsRepository.java
@@ -0,0 +1,5 @@
+package org.thenakliman.chupe.repositories;
+
+public class QuestionsRepository {
+
+}
