885163d9fc98bac7f1053750e23e693939e34f75
Rajiv Kumar <thenakliman@gmail.com>
2018-05-25 21:09:19 +0530

    [thenakliman] #22 Using postgres instead of mariadb

--- src/main/resources/db/migration/V1_1__add_question_table.sql
+++ src/main/resources/db/migration/V1_1__add_question_table.sql
@@ -1,8 +1,8 @@
 create table questions (
-    id int AUTO_INCREMENT primary key,
+    id serial primary key,
     question varchar(100) not null,
     description varchar(1000),
-    created_at TIMESTAMP default CURRENT_TIMESTAMP(),
+    created_at TIMESTAMP default CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     owner varchar(50) not null,
     assignedTO varchar(50) not null,
--- src/main/resources/db/migration/V1__Create_user.sql
+++ src/main/resources/db/migration/V1__Create_user.sql
@@ -1,23 +1,27 @@
 create table users(
-    first_name varchar(50),
-    last_name varchar(50),
-	username varchar(50) not null primary key,
-	email varchar(50),
+    first_name varchar(256),
+    last_name varchar(256),
+	username varchar(256) not null primary key,
+	email varchar(256),
 	password varchar(1024) not null,
 	enabled boolean not null
 );
 
 create table authorities (
-	username varchar(50) not null,
-	authority varchar(50) not null,
+	username varchar(256) not null,
+	authority varchar(256) not null,
 	constraint fk_authorities_users foreign key(username) references users(username)
 );
 create unique index ix_auth_username on authorities (username, authority);
 
-insert into users values('Prasun', 'hazari', 'prasun_hazari', 'phazari@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', True);
-insert into users values('Lal', 'Singh', 'lal_singh', 'lalsingh@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', True);
-insert into users values('Kulamani', 'Sethi', 'kulamani_sethi', 'ksethi@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', True);
-insert into users values('Prince', 'Bhatia', 'prince_bhatia', 'prince_bhatia@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', True);
+insert into users (first_name, last_name, username, email, password, enabled)
+            values('Prasun', 'hazari', 'prasun_hazari', 'phazari@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', 'true');
+insert into users (first_name, last_name, username, email, password, enabled)
+            values('Lal', 'Singh', 'lal_singh', 'lalsingh@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', 'true');
+insert into users (first_name, last_name, username, email, password, enabled)
+            values('Kulamani', 'Sethi', 'kulamani_sethi', 'ksethi@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', 'true');
+insert into users (first_name, last_name, username, email, password, enabled)
+            values('Prince', 'Bhatia', 'prince_bhatia', 'prince_bhatia@example.com', '$2a$04$Z8Rsh/NmQ/sdTPrcig00j.A3mZADzreAVoQx7ea9sD4rPwZRpPnMG', 'true');
 
 insert into authorities values('prasun_hazari', 'admin');
 insert into authorities values('lal_singh', 'member');
