c7fba47e60cf1558b1eb5c050173390074a1d5d0
Rajiv Kumar <thenakliman@gmail.com>
2018-05-27 11:04:22 +0530

    [thenakliman] Spring security is disabled

--- src/main/java/org/thenakliman/chupe/config/SecurityConfiguration.java
+++ src/main/java/org/thenakliman/chupe/config/SecurityConfiguration.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.thenakliman.chupe.services.TokenService;
 import org.thenakliman.chupe.services.UserService;
 
 
@@ -16,6 +18,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Autowired
   UserService userService;
+
+  @Autowired
+  TokenService tokenService;
 
   private DaoAuthenticationProvider getDaoAuthenticationProvider() {
     return new DaoAuthenticationProvider();
@@ -31,5 +36,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
     daoAuthenticationProvider.setUserDetailsService(userService);
     auth.authenticationProvider(daoAuthenticationProvider);
+  }
+
+  protected void configure(HttpSecurity http) {
+    try {
+      http
+          .authorizeRequests()
+          .antMatchers("/api/v1/**").permitAll();
+    } catch (Exception e) {
+      // todo(thenakliman) Currently security is disabled
+    }
   }
 }
