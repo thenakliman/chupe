5bd59e1714078b6238b52493e0a1e8b7ea7f779f
Rajiv Kumar <thenakliman@gmail.com>
2018-06-18 20:04:41 +0530

    [thenakliman] #26 Provided an API to fetch an answer

---
+++ src/main/java/org/thenakliman/chupe/controllers/AnswerController.java
@@ -0,0 +1,36 @@
+package org.thenakliman.chupe.controllers;
+
+import java.util.List;
+import javassist.NotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thenakliman.chupe.models.Answer;
+import org.thenakliman.chupe.services.AnswerService;
+
+@Controller
+public class AnswerController extends BaseController {
+
+  @Autowired
+  AnswerService answerService;
+
+  /** API for fetching all the answer.
+   *
+   * @param id of the question for which answer has to be fetched
+   * @return List of answers
+   * @throws NotFoundException Raises when no question is found
+   */
+  @GetMapping("/answers")
+  public ResponseEntity<List<Answer>> getAnswerOfGivenQuestion(
+          @RequestParam("questionId") long id) throws NotFoundException {
+    try {
+      return new ResponseEntity<>(answerService.getAnswersOfGivenQuestion(id), HttpStatus.OK);
+    } catch (NotFoundException ex) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  }
+}
---
+++ src/test/java/org/thenakliman/chupe/controllers/AnswerControllerTest.java
@@ -0,0 +1,93 @@
+package org.thenakliman.chupe.controllers;
+
+import static org.hamcrest.beans.SamePropertyValuesAs.samePropertyValuesAs;
+import static org.junit.Assert.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import javassist.NotFoundException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.thenakliman.chupe.models.Answer;
+import org.thenakliman.chupe.services.AnswerService;
+
+
+@WebMvcTest(controllers = AnswerController.class)
+@RunWith(SpringRunner.class)
+public class AnswerControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @MockBean
+  private AnswerService answerService;
+
+  @Autowired
+  private Jackson2ObjectMapperBuilder jacksonBuilder;
+
+  private ObjectMapper objectMapper;
+
+  /** Setup web application context. */
+  @Before()
+  public void testSetup() {
+    objectMapper = jacksonBuilder.build();
+    /* NOTE(thenakliman) this approach has been used to bypass authentication
+     * layer from the test. It creates a standalone application with single
+     * controller(QuestionController).
+     * */
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void shouldGetAnswerToGivenQuestion() throws  Exception {
+    Answer answer = new Answer();
+    int questionId = 100;
+    answer.setQuestionId(questionId);
+    answer.setAnsweredBy("user");
+    answer.setId(10);
+    answer.setAnswer("answer");
+    List<Answer> answers = new ArrayList<Answer>();
+    answers.add(answer);
+    BDDMockito.given(answerService.getAnswersOfGivenQuestion(questionId)).willReturn(answers);
+
+    MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+        .get("/api/v1/answers?questionId=" + questionId)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+
+    List<Answer> result = objectMapper.readValue(
+        mvcResult.getResponse().getContentAsString(), List.class);
+
+    assertThat(answers, samePropertyValuesAs(result));
+  }
+
+  @Test
+  public void shouldReturnNotFoundStatusCodeIfQuestionNotFound() throws  Exception {
+    int questionId = 1000;
+    BDDMockito.given(
+        answerService.getAnswersOfGivenQuestion(questionId))
+            .willThrow(new NotFoundException(questionId + " question not found"));
+
+    MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+        .get("/api/v1/answers?questionId=" + questionId)
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isNotFound()).andReturn();
+  }
+}
