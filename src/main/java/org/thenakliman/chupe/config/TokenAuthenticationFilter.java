fa8016ff4ae4425484b42e47e2f1a1d7fe07685d
Rajiv Kumar <thenakliman@gmail.com>
2018-07-24 23:09:54 +0530

    [thenakliman] #000 Changed import order

--- src/main/java/org/thenakliman/chupe/config/TokenAuthenticationFilter.java
+++ src/main/java/org/thenakliman/chupe/config/TokenAuthenticationFilter.java
@@ -1,17 +1,17 @@
 package org.thenakliman.chupe.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
-import org.thenakliman.chupe.services.TokenService;
-
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import org.thenakliman.chupe.services.TokenService;
+
 
 @Component
 public class TokenAuthenticationFilter extends GenericFilterBean {
