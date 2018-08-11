6a59e929117d59061da427a8861a4ab31514efde
Rajiv Kumar <thenakliman@gmail.com>
2018-08-11 21:00:20 +0530

    [thenakliman] #29 Create database for the team fund

---
+++ src/main/resources/db/migration/V1_3__add_team_fund_table.sql
@@ -0,0 +1,24 @@
+create table fundType (
+    id serial primary key,
+    type varchar(20),
+    default_amount int,
+    created_by varchar(256),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP default CURRENT_TIMESTAMP,
+    foreign key(created_by) references users(username)
+);
+
+create table fund (
+    id serial primary key,
+    type int,
+    owner varchar(256) not null,
+    amount int not null,
+    added_by varchar(256) not null,
+    transaction_type varchar(10),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP default CURRENT_TIMESTAMP,
+    isApproved boolean default false,
+    foreign key(added_by) references users(username),
+    foreign key(owner) references users(username),
+    foreign key(type) references fundType(id)
+);
