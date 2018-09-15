e193e246cebc2ce6cdc5cf6925cce57c895d99bf
Rajiv Kumar <thenakliman@gmail.com>
2018-09-15 22:22:38 +0530

    [thenakliman] Added table for task

---
+++ src/main/resources/db/migration/V1_4__add_task_table.sql
@@ -0,0 +1,10 @@
+create table task (
+    id serial primary key,
+    description varchar(256) not null,
+    state varchar(256) not null,
+    progress int default 0,
+    created_by varchar(256) not null,
+    created_at TIMESTAMP default CURRENT_TIMESTAMP not null,
+    updated_at TIMESTAMP not null,
+    foreign key(created_by) references users(username)
+);
