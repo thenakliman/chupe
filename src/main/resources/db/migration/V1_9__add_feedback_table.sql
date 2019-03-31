8143ed5cc78cbaaa5d994f68c09580fc13c69d01
Rajiv Kumar <thenakliman@gmail.com>
2019-03-31 12:13:11 +0530

    [thenakliman] #0000 Fix table name and add type field to feedback point

--- src/main/resources/db/migration/V1_9__add_feedback_table.sql
+++ src/main/resources/db/migration/V1_9__add_feedback_table.sql
@@ -7,11 +7,12 @@ create table feedback_session (
     foreign key(created_by) references users(username)
 );
 
-create table feedback (
+create table feedback_point (
     id serial primary key,
     description varchar(256) not null,
     given_to varchar(256) not null,
     given_by varchar(256) not null,
+    type varchar(256),
     created_at TIMESTAMP default CURRENT_TIMESTAMP not null,
     updated_at TIMESTAMP not null,
     foreign key(given_by) references users(username),
