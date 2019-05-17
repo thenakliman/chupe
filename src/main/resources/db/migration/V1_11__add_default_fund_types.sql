86eddfa7f7ba5a10b15b6c634eba192589371128
Rajiv Kumar <thenakliman@gmail.com>
2019-05-17 21:14:15 +0530

    [thenakliman] #0000 Provide default fund types

---
+++ src/main/resources/db/migration/V1_11__add_default_fund_types.sql
@@ -0,0 +1,4 @@
+insert into fund_type values(0, 'DSM_LATE_JOIN', 100, 'DSM Late joining bar', 'prasun_hazari', current_date, current_date);
+insert into fund_type values(1, 'BIRTHDAY', 1500, 'Birthday party', 'prasun_hazari', current_date, current_date);
+insert into fund_type values(2, 'ON_SITE', 2500, 'Onsite party', 'prasun_hazari', current_date, current_date);
+insert into fund_type values(3, 'MARRIAGE', 3500, 'Marriage party', 'prasun_hazari', current_date, current_date);
